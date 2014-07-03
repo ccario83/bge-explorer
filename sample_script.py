6f61d82952820585d149b483e40a1c1b6eb8546e
Clint Cario <clint.cario@ucsf.edu>
2014-07-03 16:59:23 -0700

    Updated documentation

---
+++ sample_script.py
@@ -0,0 +1,8 @@
+from BGCExplorer import *
+NRPS = BGCs(filters={'Kind':"NRPS"})  # Put your filters here (see filters.txt)
+#NRPS.compare_bgcs()                   # This will compare all filtered BGCs and store values in the DB
+#NRPS.augment()                        # Recommend but not required. Will get supplemental data on BGCs from NCBI
+NRPS.cluster(cutoff=.7)                        # Perform the clustering based on filtering criteria and similarity comparisons
+NRPS.visualize_clusters()             # Show the clustering results as a bubble plot in a web browser
+#	-- or --
+#NRPS.visualize_network()              # Show the clustering results as a network in a web browser
